91f04297c58c11dadcf0e81269d56dfd23bedce3
NikhileshJr08 <nikhileshjr08@gmail.com>
2021-05-15 14:41:54 +0530

    Update deleteProduct.js

--- server/routes/deleteProduct.js
+++ server/routes/deleteProduct.js
@@ -1,21 +1,30 @@
 const router = require('express').Router();
 const User = require('../models/User');
 
+//delete product
 router.delete('/:googleId', async (req, res)=>{
 
     try {
         // deleting the product from user collection
-        const product = await User.findOne({ googleId : req.params.googleId });
-        let product1 = product.inventory;
-        const del = product1.findOneAndDelete({ productId: req.body.productId });
-        //product.inventory.productId( req.body.productId ).remove();
-        const del1 = await product.save();
-        res.status(200).send(del1);
+        // const product = await User.findOne({ googleId : req.params.googleId });
+        // let product1 = product.inventory;
+        //const del = await product1.findOneAndDelete({ productId: req.body.productId });
+        // product.inventory.productId( req.body.productId ).remove();
+        // const del1 = await product.save();
+        // res.status(200).send(del1);
+
+
+        const doc = await User.findOne({ googleId : req.params.googleId});
+        doc.inventory.id( req.body.productId ).remove();
+        const del = await doc.save();
+        res.status(200).send(del);
+
     } catch (err) {
         res.status(400).send(err)
     }
 });
 
+//delete user
 router.delete('/user/:googleId',async (req,res)=>{
     try {
         const del = await User.findOneAndDelete({ googleId: req.params.googleId });
