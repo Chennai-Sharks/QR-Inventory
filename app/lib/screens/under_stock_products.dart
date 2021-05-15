4133c00e01366439b1eae85c115e2f1cd817f296
Poujhit <poujhit.dev@gmail.com>
2021-05-16 01:06:25 +0530

    Added understock screen

---
+++ app/lib/screens/under_stock_products.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class UnderStockProductScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
