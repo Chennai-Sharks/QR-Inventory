845b1a194a1474ca0bcb61b63abca650acb0b24d
NaveenSriram-dev <62332925+NaveenSriram-dev@users.noreply.github.com>
2021-05-16 02:11:07 +0530

    minor fix

--- server/routes/understockAlert.js
+++ server/routes/understockAlert.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User = require('../models/User');
+const User = require('../models/User').User;
 
 router.get('/:googleId', async(req,res) => {
   try {
