976c910d7b0e88fe5c9b89acbb9218dbfdb9bf46
Poujhit <poujhit.dev@gmail.com>
2021-05-16 02:35:26 +0530

    Added error handling in log.js

--- server/routes/log.js
+++ server/routes/log.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 const Logs = require('../models/User').Logs;
 
-router.get('/:googleId', async (req,res)=>{
-    doc = await Logs.findOne({ googleId: req.params.googleId}); 
-    if(doc)
-    {
-    res.send(doc.logs)
-    }
-})
+router.get('/:googleId', async (req, res) => {
+	try {
+		doc = await Logs.findOne({ googleId: req.params.googleId });
+		if (doc) {
+			res.send(doc.logs);
+		} else res.status(400).send('No logs');
+	} catch (error) {
+		console.log(error);
+		res.send(400).send('Error');
+	}
+});
 module.exports = router;
