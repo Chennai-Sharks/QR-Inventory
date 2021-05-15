2c848d091950f0acbbc4e5e7241a2794b5604a47
NikhileshJr08 <nikhileshjr08@gmail.com>
2021-05-15 21:52:29 +0530

    Error handling

--- server/routes/deleteProduct.js
+++ server/routes/deleteProduct.js
@@ -6,14 +6,17 @@ router.delete('/:googleId', async (req, res)=>{
 
     try {
         let product = await User.findOne({ googleId: req.params.googleId });
+        let flag = 0;
         for(let i=0;i<product.inventory.length;i++){
           if(product.inventory[i].productId == req.body.productId){
             product.inventory[i].remove();
             const del = await product.save();
+            flag = 1;
             res.status(200).send("Deleted");
-          }else{
-          res.status(400).send("Product does not exist");
-        }
+          }
+      }
+      if(flag==0){
+        res.status(400).send("Product does not exist");
       }
     } catch (err) {
         res.status(400).send(err)
